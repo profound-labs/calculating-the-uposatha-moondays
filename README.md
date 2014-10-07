bc6a543c93baad59f4bf87747f6fa490f0a5d729
Gambhiro Bhikkhu <gambhiro.bhikkhu.85@gmail.com>
2014-10-07 02:24:18 +0100

    ready for feedback

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 
 # Calculating the Uposatha Moondays
 
-Download the PDF from the repo.
+Download the [PDF](https://github.com/profound-labs/calculating-the-uposatha-moondays/blob/master/calculating-the-uposatha-moondays.pdf) or the whole repository as [zip archive](https://github.com/profound-labs/calculating-the-uposatha-moondays/archive/master.zip).
 
 Unresolved questions:
 
-- When is the adhikavara inserted?
-- Only Mahanikaya adds adhikavara?
+- Did 2014 have adhikavāra?
+- The formulas predict 2016 to have an adhikavāra, see \ref{adhikvara-prediction}
+- Where in the year is the adhikavāra inserted?
+- Only Mahānikāya adds adhikavāra?
+- Where is the Pāli method in sec.\ref{pali-method} used?
+- Thai text in [references folder](https://github.com/profound-labs/calculating-the-uposatha-moondays/tree/master/references): Calendar and Era use in Thailand from /The Journal of the Royal Institute of Thailand/ -- does it have relevant notes?
 
--- sources/suriya/suriya.rb
+++ sources/suriya/suriya.rb
@@ -13,22 +13,27 @@ class MyDate < Date
   end
 
   # TODO: these should be instance vars
-  def kammacubala_avoman
+  def kat
     a = (self.cs_year * 292207) + 373
     horakhun = (a/800 + 1).floor
     kammacubala = 800 - a % 800
     #uccabala = (horakhun + 2611) % 3232
     avoman = ((horakhun*11) + 650) % 692
-    [ kammacubala, avoman ]
+    thaloengsok = ((((horakhun*11) + 650) / 692).floor + horakhun) % 30
+    [ kammacubala, avoman, thaloengsok ]
   end
 
   # TODO: this is bad Ruby
   def kammacubala
-    (self.kammacubala_avoman)[0]
+    (self.kat)[0]
   end
 
   def avoman
-    (self.kammacubala_avoman)[1]
+    (self.kat)[1]
+  end
+
+  def thaloengsok
+    (self.kat)[2]
   end
 
   def is_suriya_leap?
@@ -36,7 +41,8 @@ class MyDate < Date
   end
 
   def is_adhikamat?
-    [ 1320, 1323, 1326, 1328, 1331, 1334, 1337, 1339 ].include? self.cs_year
+    t = self.thaloengsok
+    (t >= 25 && t <= 29) || (t >= 0 && t <= 6)
   end
 
   def would_be_adhikawan?
@@ -66,14 +72,15 @@ class MyDate < Date
 
 end
 
-(1958..1978).each do |y|
+(1958..2040).each do |y|
   d = MyDate.parse("#{y}-01-01")
+  type = ' '
   if d.is_adhikamat?
-    puts 'm'
+    type = 'm'
   elsif d.is_adhikawan?
-    puts 'd'
-  else
-    puts ' '
+    type = 'd'
   end
+
+  puts "#{y} #{d.cs_year} - #{type} - #{d.kat.join(',')}"
 end
 
